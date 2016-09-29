92b67a10482bdd35f60861183c3bc78bb1003321
Han <hc6h8@mail.missouri.edu>
2016-09-29 12:05:55 -0500

    Update test2.py

--- CNN/craigslist_sample/spiders/test2.py
+++ CNN/craigslist_sample/spiders/test2.py
@@ -1,3 +1,8 @@
+import csv
+import scrapy
+import re
+import os.path
+import csv
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
@@ -7,17 +12,7 @@ from craigslist_sample.items import CNNItem
 class MySpider(CrawlSpider):
     name = "cnn"
     allowed_domains = ["cnn.com"]
-    start_urls = [
-        "http://www.cnn.com/",
-        "http://www.cnn.com/health",
-        "http://www.cnn.com/money",
-        "http://www.cnn.com/opinion",
-        "http://www.cnn.com/world",
-        "http://www.cnn.com/politics",
-        "http://www.cnn.com/style",
-        "http://www.cnn.com/travel",
-        "http://www.cnn.com/sports"
-    ]
+    start_urls = [url.strip() for url in open('items.csv').readlines()]
 
     rules = (
         Rule(SgmlLinkExtractor(allow=('http://www.cnn.com/\d{4}\/\d{2}\/\d{2}\/.*\/.*\/')), callback="parse_items", follow= True),
