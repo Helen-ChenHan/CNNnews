da93d96c8888661f14d08bd39fc727f703583f4b
Han <hc6h8@mail.missouri.edu>
2016-10-14 12:19:50 -0500

    Update test2.py

--- CNN/craigslist_sample/spiders/test2.py
+++ CNN/craigslist_sample/spiders/test2.py
@@ -1,22 +1,33 @@
-import csv
 import scrapy
 import re
 import os.path
-import csv
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
 from craigslist_sample.items import CNNItem
 
-
 class MySpider(CrawlSpider):
     name = "cnn"
     allowed_domains = ["cnn.com"]
-    start_urls = [url.strip() for url in open('items.csv').readlines()]
-
-    rules = (
-        Rule(SgmlLinkExtractor(allow=('http://www.cnn.com/\d{4}\/\d{2}\/\d{2}\/.*\/.*\/')), callback="parse_items", follow= True),
-    )
+    start_urls = ['http://www.cnn.com/']
+    
+    base_url = 'http://www.cnn.com/sitemaps/sitemap-articles'
+    year = ['2016','2015','2014','2013','2012','2011']
+    month = ['12','11','10','09','08','07','06','05','04','03','02','01']
+    
+    def parse(self,response):
+        for y in self.year:
+            for m in self.month:
+                    url = self.base_url+'-'+y+'-'+m+'.xml'
+                    yield scrapy.Request(url,self.parseList)
+                    
+    def parseList(self,response):
+        nodename = 'loc'
+        text = body_or_str(response)
+        r = re.compile(r"(<%s[\s>])(.*?)(</%s>)" % (nodename, nodename), re.DOTALL)
+        for match in r.finditer(text):
+            url = match.group(2)
+            yield scrapy.Request(url,self.parse_items)
 
     def parse_items(self, response):
         hxs = HtmlXPathSelector(response)
